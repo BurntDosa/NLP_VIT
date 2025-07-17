7c46949cbd0eb208b3ef5d213bfc1f087503387f
Gagan N Bangaragiri <107536730+BurntDosa@users.noreply.github.com>
2025-07-17 11:54:11 +0530

    Added french and kannada corpus

---
+++ Task1/lol.py
@@ -0,0 +1,10 @@
+print("Im gonna read your mind")
+print("think of a number:(type it here)")
+a = input()
+print(f"you thought of {a} :o i read your mind woahhhhhh")
+
+while(True):
+    print("think of another number now")
+    b = input()
+    print(f"you thought of {b} :o i read your mind woahhhhhh")
+    
